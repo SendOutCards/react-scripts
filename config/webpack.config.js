fcd6fc2068424accbc3978cc9e0b4c46e9437704
Jared M. Peterson <jared.peterson@sendoutcards.com>
2019-01-09 10:41:49 -0700

    @emotion css-prop

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -354,7 +354,10 @@ module.exports = function(webpackEnv) {
                 // @remove-on-eject-begin
                 babelrc: false,
                 configFile: false,
-                presets: [require.resolve('babel-preset-react-app')],
+                presets: [
+                  require.resolve('@emotion/babel-preset-css-prop'),
+                  require.resolve('babel-preset-react-app'),
+                ],
                 // Make sure we have a unique cache identifier, erring on the
                 // side of caution.
                 // We remove this when the user ejects because the default
@@ -373,7 +376,6 @@ module.exports = function(webpackEnv) {
                 ),
                 // @remove-on-eject-end
                 plugins: [
-                  require.resolve('babel-plugin-emotion'),
                   [
                     require.resolve('babel-plugin-named-asset-import'),
                     {
