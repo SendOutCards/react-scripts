080fa6224129acd6379649cd566fbcac51374d7d
Jared M. Peterson <jared.peterson@sendoutcards.com>
2019-01-09 11:25:39 -0700

    class

--- template-typescript/src/components/App/App.tsx
+++ template-typescript/src/components/App/App.tsx
@@ -1,29 +1,31 @@
-import React, { FunctionComponent } from 'react'
+import React, { Component } from 'react'
 import logo from '../../assets/logo.svg'
 import styles from './styles'
 
-const App: FunctionComponent = props => {
-  return (
-    <div css={styles.App}>
-      <header css={styles.AppHeader}>
-        <img src={logo} css={styles.AppLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Version: <code>{process.env.APP_VERSION}</code>
-        </p>
-        <a
-          css={styles.AppLink}
-          href="https://github.com/SendOutCards"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          SendOutCards
-        </a>
-      </header>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div css={styles.App}>
+        <header css={styles.AppHeader}>
+          <img src={logo} css={styles.AppLogo} alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <p>
+            Version: <code>{process.env.APP_VERSION}</code>
+          </p>
+          <a
+            css={styles.AppLink}
+            href="https://github.com/SendOutCards"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            SendOutCards
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App
