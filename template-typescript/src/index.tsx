928321f3bf9be4d74c61ff894818c6624aa8dcd8
Jared M. Peterson <jared.peterson@sendoutcards.com>
2019-03-07 11:06:46 -0700

    upgrades and typescript speed-up

--- template-typescript/src/index.tsx
+++ template-typescript/src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// tslint:disable-next-line:no-import-side-effect
+import '@emotion/core'
+
 // relative imports
 import App from './components/App/App'
 
